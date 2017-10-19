4000daaa15ea427d8322af585d20ef9cb4ed63d5
danielsawin <danielsawin@gmail.com>
2017-10-19 19:17:15 -0400

    Actual functional code

--- src/ios/Watchface.swift
+++ src/ios/Watchface.swift
@@ -1,29 +1,69 @@
 //
 //  Watchface.swift
-//  
+//
 //
 //  Created by Daniel Sawin on 10/19/17.
 //
 
 import Foundation
-import "CDVPhotos.h"
-import <Photos/Photos.h>
+import Photos
 
-@objc(Watchface) class Watchface : CDVPlugin {
-    var mainAssetCollection: PHAssetCollection!
-    var oldAssetCollection: PHAssetCollection!
-    var created: bool
+
+class CustomPhotoAlbum {
+    var albumName = "Album Name"
     
-    func createAlbum(albumName: String) {
+    var assetCollection: PHAssetCollection!
+    
+    func load() {
+        if let assetCollection = fetchAssetCollectionForAlbum() {
+            self.assetCollection = assetCollection
+            return
+        }
+        
+        if PHPhotoLibrary.authorizationStatus() != PHAuthorizationStatus.authorized {
+            PHPhotoLibrary.requestAuthorization({ (status: PHAuthorizationStatus) -> Void in
+                ()
+            })
+        }
+        
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized {
+            self.createAlbum()
+        } else {
+            PHPhotoLibrary.requestAuthorization(requestAuthorizationHandler)
+        }
+    }
+    
+    func requestAuthorizationHandler(status: PHAuthorizationStatus) {
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized {
+            // ideally this ensures the creation of the photo album even if authorization wasn't prompted till after init was done
+            print("trying again to create the album")
+            self.createAlbum()
+        } else {
+            print("should really prompt the user to let them know it's failed")
+        }
+    }
+    
+    func createAlbum() {
         PHPhotoLibrary.shared().performChanges({
-            PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: albumName)   // create an asset collection with the album name
+            PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: self.albumName)   // create an asset collection with the album name
         }) { success, error in
             if success {
-              return self.fetchAssetCollectionForAlbum()
+                self.assetCollection = self.fetchAssetCollectionForAlbum()
             } else {
-                print("error \(error)")
+                print("error \(String(describing: error))")
             }
         }
+    }
+    
+    func fetchAssetCollectionForAlbum() -> PHAssetCollection? {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.predicate = NSPredicate(format: "title = %@", self.albumName)
+        let collection = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: fetchOptions)
+        
+        if let _: AnyObject = collection.firstObject {
+            return collection.firstObject
+        }
+        return nil
     }
     
     func save(image: UIImage) {
@@ -34,69 +74,62 @@ import <Photos/Photos.h>
         PHPhotoLibrary.shared().performChanges({
             let assetChangeRequest = PHAssetChangeRequest.creationRequestForAsset(from: image)
             let assetPlaceHolder = assetChangeRequest.placeholderForCreatedAsset
-            let albumChangeRequest = PHAssetCollectionChangeRequest(for: mainAssetCollection)
+            let albumChangeRequest = PHAssetCollectionChangeRequest(for: self.assetCollection)
             let enumeration: NSArray = [assetPlaceHolder!]
             albumChangeRequest!.addAssets(enumeration)
             
         }, completionHandler: nil)
     }
+    
+    func removeImages() {
+        let oldFace = PHAsset.fetchKeyAssets(in: self.assetCollection, options: nil)
+        let request = PHAssetCollectionChangeRequest(for: self.assetCollection)
+        request!.removeAssets(oldFace!)
+    }
+}
+
+@objc(Watchface) class Watchface : CDVPlugin {
+    
     func update(command: CDVInvokedUrlCommand) {
-        var pluginResult = CDVPluginResult(
+        //Fetch and Create Albums
+        let mainAlbum = CustomPhotoAlbum()
+        mainAlbum.albumName = "oneWatch"
+        mainAlbum.load()
+        let oldAlbum = CustomPhotoAlbum()
+        oldAlbum.albumName = "oneWatch Archive"
+        oldAlbum.load()
+        
+        let pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
         
-        let dataURL = command.arguments[0] as? String ?? ""
+        let dataURL = command.arguments[0]
         //Create image with DataURL
-        let imageData = NSData(base64EncodedStringL dataURL options: .allZeros)
+        let imageData = NSData(base64Encoded: dataURL as! String, options: [])
         
-        let newFace = UIImage(data: imageData)
+        let newFace = UIImage(data: imageData! as Data)
         
-        let mainAlbumName = "oneWatch"
-        let oldAlbumName = "oneWatch Archive"
         
         //Check if folders exist
-        //Fetch Main Album
-        let fetchOptions = PHFetchOptions();
-        fetchOptions.predicate = NSPredicate(format: "title = %@", mainAlbumName)
-        
-        let mainAlbum = PHAssetCollection.fetchAssetCollectionsWithType(.Album, subtype: .Any, options: fetchOptions);
-        
-        //Fetch Old Album
-        fetchOptions = PHFetchOptions()
-        fetchOptions.predicate = NSPredicate(format: "title = %@", oldAlbumName)
-        
-        let oldAlbum = PHAssetCollection.fetchAssetCollectionsWithType(.Album, subtype: .Any, options: fetchOptions);
-        
-        //Check if Main Album Exists
-        if(mainAlbum == nil){
-            created = false
-            //Create Album
-            mainAssetCollection = createAlbum(mainAlbumName);
-            //Put Image in Album
-            save(newFace)
+        if(mainAlbum.fetchAssetCollectionForAlbum() == nil){
+            mainAlbum.createAlbum()
+            oldAlbum.createAlbum()
         }else{
-            created = true
-            //Main Album Exists
-            mainAssetCollection = mainAlbum.firstObject as PHAssetCollection
-            
-            //Move assets from Main Album to Old AlbumAlbum
-            var oldFac: PHFetchResult
-            oldFace = fetchKeyAssets(in mainAlbumName, nil)
-            
-            PHAssetCollectionChangeRequest(forAssetCollection: mainAssetCollection).removeAssets(oldFace)
-            PHAssetCollectionChangeRequest(forAssetCollection: oldAssetCollection).addAssets(oldFace)
+            oldAlbum.removeImages()
         }
         
-        //Check if Old Album Exists
-        if(oldAlbum == nil){
-            //Create Album
-            oldAssetCollection = createAlbum(oldAlbumName)
+        if(oldAlbum.fetchAssetCollectionForAlbum() == nil){
+            oldAlbum.createAlbum()
         }else{
-            //Old Album Exists
-            oldAssetCollection = oldAlbum.firstObject as PHAssetCollection
-            //Put New Face in Main Album
-            save(newFace)
+            mainAlbum.save(image: newFace!)
         }
+        
+        //Send pluginResult
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+        
     }
-    
 }
+
